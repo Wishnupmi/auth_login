e41918a19e30db0784292d44b1683830c2a307b5
wishnuprogramming <wishnupmi@gmail.com>
2021-08-26 19:40:43 +0700

    first commit

---
+++ js/my-app2.js
@@ -0,0 +1,157 @@
+// Dom7
+var $ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  id: 'io.framework7.testapp',
+  root: '#app',
+  name: 'Myapp',
+  view: {
+    stackPages:true,
+  },
+  theme: theme,
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+     methods: {
+      signIn: function () {
+        var $ = this.$;
+        var app = this.$app;
+        var username = $('input#demo-username-1').val();
+        var password = $('input#demo-password-1').val();
+        app.dialog.alert('Username: ' + username + '<br>Password: ' + password, function () {
+          app.loginScreen.close();
+        })
+      }
+    },
+  routes: routes,
+  popup: {
+    closeOnEscape: true,
+  },
+  sheet: {
+    closeOnEscape: true,
+  },
+  popover: {
+    closeOnEscape: true,
+  },
+  actions: {
+    closeOnEscape: true,
+  },
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
+  },
+});
+
+var mainView = app.views.create('.view-main');
+var $$ = Dom7;
+
+
+
+cek();
+
+function cek(){
+  var status = localStorage.getItem("status");
+  if (status=="login") {
+     // app.dialog.alert("Berhasil Login");
+
+     // remove clear hanya sementara
+    // localStorage.removeItem("status");
+    // localstorage.clear();
+
+    app.views.main.router.navigate('/dashboard/');
+
+  } else {
+    // app.views.main.router.navigate('/home/');
+  }
+}
+
+
+
+
+    
+
+
+$$("#masuk").click(function(){
+  var username = $$("#username").val();
+  var password = $$("#password").val();
+
+// app.request({
+//   url: 'http://localhost/baru/login.php',
+//   statusCode: {
+//     404: function (xhr) {
+//       alert('page not found');
+//     }
+//   }
+// })
+
+// app.dialog.alert("tes","Konfirmasi");
+  app.request({
+    // url: "http://localhost/baru/login.php",
+    url: "http://bloodhum.com/core/login.php",
+    type: "POST",
+    data: {
+      "username": username,
+      "password": password,
+    },
+    dataType: 'json',
+    success:function(data){
+
+      console.log(data);
+      
+      if(data.error){
+        app.dialog.alert(data.pesan,"Konfirmasi");
+
+      } else {
+        // localStorage.setItem("status","login");
+        // localStorage.setItem("id",data[0].id);
+        // localStorage.setItem("username",data[0].Email);
+        // app.dialog.alert(data.pesan);
+
+        // app.dialog.preloader('My text...');
+
+       
+        app.dialog.preloader();
+        
+
+  setTimeout(function () {
+    // localStorage.setItem("status","login");
+    //     localStorage.setItem("id",data[0].id);
+    //     localStorage.setItem("username",data[0].Email);
+    // app.dialog.close();
+    app.dialog.close();
+
+    localStorage.setItem("status","login");
+
+    app.views.main.router.navigate('/dashboard/');
+
+  }, 1000);
+// localStorage.setItem("id",data[0].id);
+//     localStorage.setItem("username",data[0].Email);
+        
+        //cek();
+      }
+    }
+
+  });
+
+});
+
+
+
+ 
---
+++ login.php
@@ -0,0 +1,94 @@
+<?php
+// include("koneksi.php");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
+header("Access-Control-Max-Age: 604800");
+header("Access-Control-Request-Headers: x-requested-with");
+header("Access-Control-Allow-Headers: x-requested-with, x-requested-by");
+
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+
+// $qry = mysqli_query($koneksi,"select * from mahasiswa WHERE Email='$username' and password='$password'") or die (mysqli_error());
+// $data = mysqli_fetch_array($qry);
+// $banyak = mysqli_num_rows($qry);
+
+// if($banyak>=1) {
+//     $output['pesan'] = "Berhasil masuk";
+//     $output['id']=$data['id'];
+//     $output['username']=$data['Email'];
+// } else {
+//     $output['error'] = true;
+//     $output['pesan'] = "Gagal masuk";
+// }
+// echo json_encode($output);
+
+
+
+// $username = "wishnupmi@gmail.com";
+// $password = "123";
+
+
+
+
+
+
+
+// Kode Utama jangan di hapus
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+
+// $sql = "SELECT * FROM akun WHERE Email='$username' and password='$password'";
+// $result = $conn->query($sql);
+// $data = mysqli_fetch_array($qry);
+
+// if ($result->num_rows > 0) {
+//   // output data of each row
+//   while($row = $result->fetch_assoc()) {
+    
+//     $output['pesan'] = "Berhasil masuk";
+//     $output['id']=$row["Id"];
+//     $output['username']=$row['Email'];
+
+//   }
+// } else {
+//   // echo "0 results";
+//   $output['error'] = true;
+//   $output['pesan'] = "Gagal masuk";
+// }
+// echo json_encode($output);
+
+
+
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$database = file_get_contents("akun.json");
+$database = json_decode($database, TRUE);
+if(isset($username) && isset($password)) {
+    for($i=0; $i < count($database); $i++) {
+        // echo $database[$i]['username'];
+        if($database[$i]['username'] == $username) {
+            if($database[$i]['password'] == $password) {
+                $success = TRUE;
+                $output['pesan'] = "Berhasil masuk";
+                $output['id']=$database[$i]['id'];
+                $output['username']=$database[$i]['username'];
+                break;
+            } else {
+                $success = FALSE;
+                $output['error'] = true;
+                $output['pesan'] = "Gagal masuk";
+            }
+        } else {
+            $success = FALSE;
+        }
+    }
+} else {
+    echo "Harap isi semua kolom yang tersedia";
+}
+echo json_encode($output);
+
+?>
